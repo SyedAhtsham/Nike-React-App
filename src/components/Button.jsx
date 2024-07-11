10942c605be47de10568ccd0e42f626101485004
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-07-10 22:29:59 -0400

    Create the Navbar component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+      <button className=''>
+          Click me
+    </button>
+  )
+}
+
+export default Button
---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+
+export {
+    Hero, CustomerReviews, PopularProducts, SuperQuality, SpecialOffers, Services, Subscribe, Footer   
+}
