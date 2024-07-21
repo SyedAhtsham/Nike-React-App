416272386dfe4e9c58ad05266a5c3641ae635bed
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-07-21 01:18:16 -0400

    Change color on hover on the nabar links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
                         <li key={item.label} >
                             <a href='item.href'
                                 className='font-montserrat leading-normal text-lg
-                                text-slate-gray'>
+                                text-slate-gray hover:text-coral-red'>
                                 {item.label}
 
                             </a>
