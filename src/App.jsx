791dcfeb886342d6bbcc2a433553cc4deede501b
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-07-21 04:07:18 -0400

    Create routes to handle multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,18 @@
-import {
-    CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffers, Subscribe, SuperQuality
-} from './sections';
-import Nav from './components/Nav.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ViewShoe from "./pages/ViewShoe";
 
 
 const App = () => (
-
-
-
-
-
-
-
-
-    <main className="relative">
-
-       
-
-        <Nav/>
-        <section className="xl:padding-l wide:padding-r padding-b">
-            <Hero />
-        </section>
-        <section className="padding">
-            <PopularProducts />
-        </section>
-        <section className="padding">
-            <SuperQuality />
-        </section>
-        <section className="padding">
-            <Services />
-        </section>
-        <section className="padding">
-
-
-            <SpecialOffers />
-        </section>
-        <section className="bg-pale-blue padding">
-            <CustomerReviews />
-        </section>
-        <section className="padding-x sm:py-32 py-16 w-full">
-            <Subscribe />
-        </section>
-        <section className="bg-black padding-x padding-t pb-8">
-            <Footer />
-        </section>
-
-
-    </main>
-
-
+    <div>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={< Home />} />
+                <Route path="/home" element={< Home />} />
+                <Route path="/viewshoe" element={<ViewShoe />} />
+            </Routes>
+        </BrowserRouter>
+    </div>
 
 );
 
---
+++ src/pages/ViewShoe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewShoe = () => {
+  return (
+    <div>viewPage</div>
+  )
+}
+
+export default ViewShoe
