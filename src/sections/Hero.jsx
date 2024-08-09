f4727dc25424885c7e077b830ac19b27c3338000
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-09 00:44:15 -0400

    Style the hero section for mobile view

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,12 +20,12 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[58px] max-sm:leading-[62px] font-bold">
+        <h1 className="mt-8 font-palanquin text-8xl max-sm:text-[62px] max-sm:leading-[62px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
           <span className="text-coral-red inline-block mt-3"> Nike </span> Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-6 mt-6 mb-10 sm:max-w-sm">
+        <p className="font-montserrat text-slate-gray text-lg leading-6 mt-6 mb-8 sm:max-w-sm">
           Discover stylis Nike arrivals, quality comfort, and innovation for your active life.
         </p>
 
